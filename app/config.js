e62a5340da152198ce5c5a21d8549ecf90e4aab1
thcheetah777 <thcheetah777@gmail.com>
2022-03-11 17:17:54 -0800

    Init project

---
+++ app/config.js
@@ -0,0 +1,29 @@
+// Pie Dodgeball
+/*^*^*^*^*^*^*^*
+config.js
+The configuration for the Phaser 3 framework.
+*^*^*^*^*^*^*^*/
+
+const config = {
+  type: Phaser.AUTO,
+  scale: {
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    mode: Phaser.Scale.RESIZE
+  },
+  render: {
+    pixelArt: true
+  },
+  backgroundColor: 0x000000,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: {
+        y: 1500
+      },
+      enableBody: true,
+      // debug: true
+    }
+  },
+  scene: [Game]
+};
+const phaserGame = new Phaser.Game(config);
---
+++ app/script.js
@@ -0,0 +1,20 @@
+// Pie Dodgeball
+/*^*^*^*^*^*^*^*
+script.js
+The main script for Pie Dodgeball.
+*^*^*^*^*^*^*^*/
+
+class Game extends Phaser.Scene {
+  constructor() {
+    super();
+  }
+  preload() {
+
+  }
+  create() {
+
+  }
+  update() {
+
+  }
+}
