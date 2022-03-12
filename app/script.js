f2dc47611a06d83060872b9ff2da51b2e92014fa
thcheetah777 <thcheetah777@gmail.com>
2022-03-11 17:48:05 -0800

    Add boundaries

--- app/script.js
+++ app/script.js
@@ -12,12 +12,22 @@ class Game extends Phaser.Scene {
   preload() {
     // Load assets
     this.load.image("player", "assets/player.png");
+    this.load.image("boundaries", "assets/boundaries.png");
   }
   create() {
     game.engine = new Engine(this);
 
     // Create player
-    game.player = this.physics.add.sprite(game.engine.gameWidthCenter, game.engine.gameHeightCenter, "player").setScale(8).setGravityY(-1500).setDrag(500);
+    game.player = this.physics.add.sprite(game.engine.gameWidthCenter, 3 * (game.engine.gameHeight / 4), "player").setScale(8).setGravityY(-1500).setDrag(500).setSize(5, 3).setOffset(0, 0);
+
+    // Create boundary
+    game.boundaries = this.physics.add.staticGroup();
+    for (var i = 0; i < 400 * Math.round(game.engine.gameWidth / 400) + 1; i += 400) {
+      game.boundaries.create(i, game.engine.gameHeightCenter, "boundaries").setScale(8).setSize(400, 8).setOffset(0, -8);
+    }
+
+    // ********** Colliders **********
+    this.physics.add.collider(game.player, game.boundaries);
   }
   update() {
     // Player movement
