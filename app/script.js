cab74ebf97b28ba833b2b73a8601bbefd1caf20a
thcheetah777 <thcheetah777@gmail.com>
2022-03-11 18:25:03 -0800

    Throw pies

--- app/script.js
+++ app/script.js
@@ -4,7 +4,9 @@ script.js
 The main script for Pie Dodgeball.
 *^*^*^*^*^*^*^*/
 
-let game = {};
+let game = {
+  pieAngle: 0
+};
 class Game extends Phaser.Scene {
   constructor() {
     super();
@@ -13,21 +15,31 @@ class Game extends Phaser.Scene {
     // Load assets
     this.load.image("player", "assets/player.png");
     this.load.image("boundaries", "assets/boundaries.png");
+    this.load.image("pie", "assets/pie.png");
   }
   create() {
     game.engine = new Engine(this);
+
+    // Create groups
+    game.boundaries = this.physics.add.staticGroup();
+    game.pies = this.physics.add.group();
 
     // Create player
     game.player = this.physics.add.sprite(game.engine.gameWidthCenter, 3 * (game.engine.gameHeight / 4), "player").setScale(8).setGravityY(-1500).setDrag(500).setSize(5, 3).setOffset(0, 0);
 
     // Create boundary
-    game.boundaries = this.physics.add.staticGroup();
     for (var i = 0; i < 400 * Math.round(game.engine.gameWidth / 400) + 1; i += 400) {
       game.boundaries.create(i, game.engine.gameHeightCenter, "boundaries").setScale(8).setSize(400, 8).setOffset(0, -8);
     }
 
     // ********** Colliders **********
     this.physics.add.collider(game.player, game.boundaries);
+
+    // ********** Interaction **********
+    this.input.on("pointerdown", () => {
+      let pie = game.pies.create(game.player.x, game.player.y, "pie").setScale(8).setGravityY(-1500).setSize(6, 4).setOffset(0, 0);
+      this.physics.velocityFromAngle(game.pieAngle, 500, pie.body.velocity);
+    });
   }
   update() {
     // Player movement
@@ -43,5 +55,6 @@ class Game extends Phaser.Scene {
     if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) {
       game.player.setVelocityY(-300);
     }
+    game.pieAngle += 3;
   }
 }
