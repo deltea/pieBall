b6593465c1b3dadc3a61d26c581bff9d11c8846e
thcheetah777 <thcheetah777@gmail.com>
2022-03-12 17:39:41 -0800

    Shoot speed

--- app/script.js
+++ app/script.js
@@ -6,7 +6,8 @@ The main script for Pie Dodgeball.
 
 let game = {
   pieAngle: 0,
-  pieDir: -3
+  pieDir: -2,
+  holdDur: 0
 };
 class Game extends Phaser.Scene {
   constructor() {
@@ -18,16 +19,21 @@ class Game extends Phaser.Scene {
     this.load.image("boundaries", "assets/boundaries.png");
     this.load.image("pie", "assets/pie.png");
     this.load.image("arrow", "assets/arrow.png");
+    this.load.image("meter", "assets/meter.png");
+    this.load.image("meterIndicator", "assets/meterIndicator.png");
   }
   create() {
     game.engine = new Engine(this);
+
+    // Mouse input
+    game.engine.mouseInput();
 
     // Create groups
     game.boundaries = this.physics.add.staticGroup();
     game.pies = this.physics.add.group();
 
     // Create player
-    game.player = this.physics.add.sprite(game.engine.gameWidthCenter, 3 * (game.engine.gameHeight / 4), "player").setScale(8).setGravityY(-1500).setDrag(500).setSize(5, 3).setOffset(0, 0);
+    game.player = this.physics.add.sprite(game.engine.gameWidthCenter, 3 * (game.engine.gameHeight / 4), "player").setScale(8).setGravityY(-1500).setDrag(500).setSize(5, 3).setOffset(0, 0).setCollideWorldBounds(true);
 
     // Create boundary
     for (var i = 0; i < 400 * Math.round(game.engine.gameWidth / 400) + 1; i += 400) {
@@ -35,7 +41,11 @@ class Game extends Phaser.Scene {
     }
 
     // Create arrow
-    game.arrow = this.physics.add.sprite(game.player.x - 10, game.player.y - 20, "arrow").setScale(8).setGravityY(-1500);
+    game.arrow = this.physics.add.staticSprite(game.player.x - 10, game.player.y - 20, "arrow").setScale(8).setGravityY(-1500);
+
+    // Create meter
+    game.meter = this.add.image(120, game.engine.gameHeightCenter, "meter").setScale(8).setDepth(1);
+    game.meterIndicator = this.add.image(120, game.engine.gameHeightCenter + 256, "meterIndicator").setScale(8).setDepth(1);
 
     // ********** Colliders **********
     this.physics.add.collider(game.player, game.boundaries);
@@ -43,7 +53,9 @@ class Game extends Phaser.Scene {
     // ********** Interaction **********
     this.input.on("pointerup", () => {
       let pie = game.pies.create(game.player.x, game.player.y, "pie").setScale(8).setGravityY(-1500).setSize(6, 4).setOffset(0, 0);
-      this.physics.velocityFromAngle(game.pieAngle, 500, pie.body.velocity);
+      this.physics.velocityFromAngle(game.pieAngle, game.holdDur * 1.5, pie.body.velocity);
+      game.meterIndicator.y = game.engine.gameHeightCenter + 256;
+      game.holdDur = 0;
     });
   }
   update() {
@@ -59,6 +71,10 @@ class Game extends Phaser.Scene {
     }
     if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown) {
       game.player.setVelocityY(-300);
+    }
+    if (game.engine.mouseDown && game.holdDur < 500) {
+      game.holdDur += 5;
+      game.meterIndicator.y = (game.engine.gameHeightCenter + 256) - game.holdDur;
     }
     game.pieAngle += game.pieDir;
     game.arrow.x = game.player.x - 10;
